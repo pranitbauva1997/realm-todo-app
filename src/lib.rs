9b8e5554213c2c7fa1227e74a8d85ee6e97a463e
Pranit Bauva <pranit.bauva@gmail.com>
2020-06-19 13:14:02 +0530

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate serde_derive;
+
+#[macro_use]
+extern crate askama;
+
+#[macro_use]
+extern crate realm_macros;
+
+#[macro_use]
+extern crate diesel;
+
+pub mod db;
+pub mod reverse;
+pub mod routes;
+pub mod schema;
